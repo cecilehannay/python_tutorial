3c437525bb0fe67b56199b437f4d9985f5602318
Cecile Hannay <hannay@ucar.edu>
2021-04-15 18:44:51 -0600

    Output formatting comparison data

--- readfile.py
+++ readfile.py
@@ -1,10 +1,10 @@
 
 # Columns names and columns indices
-columns = {'date':0, 'time':1, 'temperature':2, 'windspeed':7}
+columns = {'date':0, 'time':1, 'temperature':2, 'windspeed':7, 'windchill':12}
 
 
 # Data types for each column (only if non-string)
-types = {'temperature': float, 'windspeed':float}
+types = {'temperature': float, 'windspeed':float, 'windchill':float}
 
 # Initialize my data variable   
 data = {}
@@ -48,7 +48,13 @@ for temperature, windspeed in zip(data['temperature'], data['windspeed']):
     windchill.append(compute_windchill(temperature, windspeed))
 
 
-# Debug
-print(windchill)
 
+# Output comparison of data
+print('                ORIGINAL  COMPUTED')
+print(' DATE    TIME  WINDCHILL WINDCHILL DIFFERENCE')
+print('------- ------ --------- --------- ----------')
+zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
+for date, time, wc_orig, wc_comp in zip_data:
+   wc_diff = wc_orig - wc_comp
+   print(f'{date} {time:>6} {wc_orig:9.6f} {wc_comp:9.6f} {wc_diff:10.6f}')
 
