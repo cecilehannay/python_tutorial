2cabdd85a438cf23ad971143a7d4009bf8dcb8f5
Cecile Hannay <hannay@ucar.edu>
2021-04-15 18:09:34 -0600

    Parsing file

--- readfile.py
+++ readfile.py
@@ -1,11 +1,24 @@
+
+# data is the whole table
+# datum is going to be each line of the table
+
+# Initialize my data variable   
+data = []
+
 # read the data file
 filename = "data/wxobs20170821.txt"
 datafile = open(filename, 'r')
 
-data = datafile.read()
+# Read the first 3 lines
+for _ in range(3):
+    datafile.readline()
+
+# read and parse the rest of the file
+for iline in datafile:
+    datum = iline.split()
+    data.append(datum)
 
 datafile.close()
 
-# DEBUG 
-print(type(data))
+
 
