fc7a943aa558b38a59ecc2b6e0ac172b58632caf
Cecile Hannay <hannay@ucar.edu>
2021-04-19 16:22:36 -0600

    Updating new heat index script

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -1,10 +1,10 @@
 
 # Columns names and columns indices
-columns = {'date':0, 'time':1, 'temperature':2, 'windspeed':7, 'windchill':12}
+columns = {'date':0, 'time':1, 'temperature':2, 'humidity':5, 'heatindex':13}
 
 
 # Data types for each column (only if non-string)
-types = {'temperature': float, 'windspeed':float, 'windchill':float}
+types = {'temperature': float, 'humidity':float, 'heatindex':float}
 
 # Initialize my data variable   
 data = {}
@@ -30,31 +30,40 @@ for line in datafile:
 
 datafile.close()
 
-# Compute the wind chill temperature
-def compute_windchill(t, v):
-   a = 35.74
-   b = 0.6215
-   c = 35.75
-   d = 0.4275
 
-   v2 = v ** 2
-   wci = a + (b * t) - (c * v2) + (d * t * v2)
-   return wci
+# Compute the heat index
+def compute_heatindex(t, rh_pct):
+    a = -42.379
+    b = 2.04901523
+    c = 10.14333127
+    d = -0.22475541
+    e = -0.00683783
+    f = -0.05481717
+    g = 0.00122874
+    h = 0.00085282
+    i = -0.00000199
+
+    rh = rh_pct / 100
+
+    hi = a + (b * t) + (c * rh) + (d * t * rh)
+    + (e * t**2) + (f * rh**2) + (g * t**2 * rh)
+    + (h * t * rh**2) + (i * t**2 * rh**2)
+    return hi
 
 
-# Compute wind chill factor
-windchill = []
-for temperature, windspeed in zip(data['temperature'], data['windspeed']):
-    windchill.append(compute_windchill(temperature, windspeed))
+# Compute heat index
+heatindex = []
+for temperature, humidity in zip(data['temperature'], data['humidity']):
+    heatindex.append(compute_heatindex(temperature, humidity))
 
 
 
 # Output comparison of data
 print('                ORIGINAL  COMPUTED')
-print(' DATE    TIME  WINDCHILL WINDCHILL DIFFERENCE')
+print(' DATE    TIME  HEATINDEX HEATINDEX DIFFERENCE')
 print('------- ------ --------- --------- ----------')
-zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
-for date, time, wc_orig, wc_comp in zip_data:
-    wc_diff = wc_orig - wc_comp
-    print(f'{date} {time:>6} {wc_orig:9.6f} {wc_comp:9.6f} {wc_diff:10.6f}')
+zip_data = zip(data['date'], data['time'], data['heatindex'], heatindex)
+for date, time, hi_obs, hi_comp in zip_data:
+    hi_diff = hi_obs - hi_comp
+    print(f'{date} {time:>6} {hi_obs:9.6f} {hi_comp:9.6f} {hi_diff:10.6f}')
 
