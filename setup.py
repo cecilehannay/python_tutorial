94a6953115ae65b51af80e89552436d94e418000
Cecile Hannay <hannay@ucar.edu>
2021-04-20 10:01:01 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="myutils",
+    version="1.0.0",
+    description="myutils package",
+    author="Cecile Hannay",
+    author_email="hannay@ucar.edu",
+    packages=["myutils"],
+    install_requires=[],
+)
